9b6607ebaa77ef620f12e4f88b555789d60c2dc4
AngelicaTeixeiraa <engelicagamers@gmail.com>
2023-05-08 21:48:33 -0300

    Classe produto

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 
-
 require "./php/Produto.php";
 
-$produto1 = new Produto();
+ $produto1 = new Produto();
 $produto1->setNome("Camiseta estampada");
  $produto1->setMarca("Gucci");
 $produto1->setdescricao("camiseta estampada da marca gucci");
-$produto1->setValor(500, 00);
- $produto1->imprimir();
-
+$produto1->setValor(5, 00);
+$produto1->imprimir();
 
 
